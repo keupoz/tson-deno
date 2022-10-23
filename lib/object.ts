4981e5f71a5938c3be4bb6a94470eccd30401e8b
Ivan Sokolov <keupoz@gmail.com>
2022-10-23 21:49:35 +0700

    Add JsonObject get shorthands

--- lib/object.ts
+++ lib/object.ts
@@ -1,4 +1,10 @@
-import { JsonElement, parseRaw } from "./index.ts";
+import {
+  JsonArray,
+  JsonElement,
+  JsonNull,
+  JsonPrimitive,
+  parseRaw,
+} from "./index.ts";
 
 export class JsonObject extends JsonElement<Record<string, unknown>> {
   public keys(): string[] {
@@ -25,6 +31,22 @@ export class JsonObject extends JsonElement<Record<string, unknown>> {
     }
 
     return parseRaw(`${this.path}[${key}]`, value);
+  }
+
+  public getArray(key: string): JsonArray {
+    return this.get(key).asArray();
+  }
+
+  public getObject(key: string): JsonObject {
+    return this.get(key).asObject();
+  }
+
+  public getPrimitve(key: string): JsonPrimitive {
+    return this.get(key).asPrimitive();
+  }
+
+  public getNull(key: string): JsonNull {
+    return this.get(key).asNull();
   }
 
   public *[Symbol.iterator](): IterableIterator<
